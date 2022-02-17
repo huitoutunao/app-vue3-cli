0278b49f5850ea8c697d0225194b5c027d5512c1
huitoutunao <lh_huitoutunao@163.com>
2022-02-17 16:05:42 +0800

    feat: 更新资源文件目录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img
       alt="Vue logo"
-      src="../assets/logo.png"
+      src="../assets/images/logo.png"
     >
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
