01ab5da9a5d2ff95650331bc2562fb2ab8556e93
huitoutunao <lh_huitoutunao@163.com>
2022-02-24 11:43:17 +0800

    feat: 更新环境配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 // const path = require('path')
 const IS_PROD = ['production'].includes(process.env.NODE_ENV)
-// const IS_DEV = ['development'].includes(process.env.NODE_ENV)
-console.log(process.env.NODE_ENV)
-console.log(IS_PROD, process.env.VUE_APP_PUBLIC_PATH)
 
 module.exports = {
   publicPath: IS_PROD ? './' : process.env.VUE_APP_PUBLIC_PATH,
